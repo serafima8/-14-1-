812e2b4f25342a42463457ee74524bc7aab70cb5
serafima8 <147659413+serafima8@users.noreply.github.com>
2024-01-18 21:08:05 +0200

    Add files via upload

---
+++ ConsoleApp1/Point.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Point
+    {
+        /// <summary>
+        /// создание поля x
+        /// </summary>
+        private double x;
+        /// <summary>
+        /// создание поля y
+        /// </summary>
+        private double y;
+
+        /// <summary>
+        /// поле для подсчета количества созданных экземпляров класса
+        /// </summary>
+        public static double count;
+
+        /// <summary>
+        /// поле метода доступа к внутренним переменным x, y
+        /// </summary>
+        public double X
+        { get { return x; } set { x = value; } }
+        public double Y
+        { get { return y; } set { y = value; } }
+
+
+
+
+        /// <summary>
+        /// конструктор с двумя параметрами, которые инициализируют одноименные внутренние поля класса
+        /// </summary>
+        public Point(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+            count++;
+        }
+        /// <summary>
+        /// Конструктор по умолчанию, который инициализирует внутренние поля 
+        /// </summary>
+        public Point()
+        {
+            x = y = 1.0;
+            count++;
+
+        }
+        /// <summary>
+        /// Статический конструктор, который инициализирует статическую переменную count нулевым значением
+        /// </summary>
+        static Point()
+        {
+            count = 0;
+        }
+        /// <summary>
+        /// Метод, который выводит информацию
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine($"x = {x}\ty = {y}\tcount = {count}");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new Point(1.6, 4.7);
+            point.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
